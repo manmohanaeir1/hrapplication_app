c6a6e538a25fc9715bf0fa37c44a03b5b109750f
Manmohan Aeir <manmohanaeir001110@gmail.com>
2022-12-09 11:08:15 +0545

    index part set limit and pagination prev and next button working

--- header.php
+++ header.php
@@ -7,6 +7,72 @@
     <!-- Bootstrap Core Css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <style>
+			html {
+				font-family: Tahoma, Geneva, sans-serif;
+				padding: 20px;
+				background-color: #F8F9F9;
+			}
+			table {
+				border-collapse: collapse;
+				width: 500px;
+			}
+			td, th {
+				padding: 10px;
+			}
+			th {
+				background-color: #54585d;
+				color: #ffffff;
+				font-weight: bold;
+				font-size: 13px;
+				border: 1px solid #54585d;
+			}
+			td {
+				color: #636363;
+				border: 1px solid #dddfe1;
+			}
+			tr {
+				background-color: #f9fafb;
+			}
+			tr:nth-child(odd) {
+				background-color: #ffffff;
+			}
+			.pagination {
+				list-style-type: none;
+				padding: 10px 0;
+				display: inline-flex;
+				justify-content: space-between;
+				box-sizing: border-box;
+			}
+			.pagination li {
+				box-sizing: border-box;
+				padding-right: 10px;
+			}
+			.pagination li a {
+				box-sizing: border-box;
+				background-color: #e2e6e6;
+				padding: 8px;
+				text-decoration: none;
+				font-size: 12px;
+				font-weight: bold;
+				color: #616872;
+				border-radius: 4px;
+			}
+			.pagination li a:hover {
+				background-color: #d4dada;
+			}
+			.pagination .next a, .pagination .prev a {
+				text-transform: uppercase;
+				font-size: 12px;
+			}
+			.pagination .currentpage a {
+				background-color: #518acb;
+				color: #fff;
+			}
+			.pagination .currentpage a:hover {
+				background-color: #518acb;
+			}
+			</style>
 
 </head>
 
--- index.php
+++ index.php
@@ -2,99 +2,143 @@
 require("header.php");
 ?>
 
-        <div class="row mt-3">
-            <div class="col-md-12">
-                <div class="card text-center active">
-                    <div class="card-header bg-info">
-                        JOBS DESCRIPTION
+<div class="row mt-3">
+    <div class="col-md-12">
+        <div class="card text-center active">
+            <div class="card-header bg-info">
+                JOBS DESCRIPTION
 
+            </div>
+            <div class="card-body">
+                <h5 class="card-title"></h5>
+                <div class="row">
+                    <div class="col-md-4">
+                        <ul class="list-group">
+
+                            <li class="list-group-item  bg-secondary" aria-current="true">Jobs items</li>
+                            <select name="limit-records" id="limit-records">
+                                <option disabled="disabled" selected="selected">---Limit Records---</option>
+                                <?php foreach ([10,20,30,40,50] as $limit) : ?>
+                                    <option <?php if (isset($_POST["limit-records"]) && $_POST["limit-records"] == $limit) echo "selected" ?> value="<?= $limit; ?>"><?= $limit; ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                            <?php
+                            include('connect.php');
+                            $limit = isset($_POST["limit-records"]) ? $_POST["limit-records"] : 5;
+                            $page = isset($_GET['page']) ? $_GET['page'] : 1;
+                            $start = ($page - 1) * $limit;
+                            $result = $con->query("SELECT * FROM hr_jobs LIMIT $start, $limit");
+                            $jobs = $result->fetchAll();
+
+                            $result1 = $con->query("SELECT count(job_id) AS id FROM hr_jobs");
+                            $jobtCount = $result1->fetchAll();
+                            $total = $jobtCount[0]['id'];
+                            $pages = ceil($total / $limit);
+
+                            $Previous = $page - 1;
+                            $Next = $page + 1;
+
+
+                            foreach ($jobs as $job) { ?>
+                                <li class="list-group-item"><?php echo $job['job_name'] . "-" .  $job['job_code']; ?></li>
+
+
+                        </ul>
+                    <?php
+                            } ?>
+
+                    <div class="col-md-10">
+                        <nav aria-label="Page navigation">
+                            <ul class="pagination">
+                                <li>
+                                    <a href="index.php?page=<?= $Previous; ?>" aria-label="Previous">
+                                        <span aria-hidden="true">&laquo; Previous</span>
+                                    </a>
+                                </li>
+                                <?php for ($i = 1; $i <= $pages; $i++) : ?>
+                                    <li><a href="index.php?page=<?= $i; ?>"><?= $i; ?></a></li>
+                                <?php endfor; ?>
+                                <li>
+                                    <a href="index.php?page=<?= $Next; ?>" aria-label="Next">
+                                        <span aria-hidden="true">Next &raquo;</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
                     </div>
-                    <div class="card-body">
-                        <h5 class="card-title"></h5>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <ul class="list-group">
-                                
-                                    <li class="list-group-item  bg-secondary" aria-current="true">Jobs items</li>
-                                    <?php
-                                    include('connect.php');
-                                    $display = $con->prepare("SELECT * FROM hr_jobs ORDER BY job_id ASC");
-                                    $display->execute();
-                                    $fetch = $display->fetchAll();
+                    </div>
+                    <div class="col-md-4">
+                        <ul class="list-group">
 
-                                    foreach($fetch as $key => $row) { ?>
-                                    <li class="list-group-item"><?php echo $row['job_name'] ."-".  $row['job_code']; ?></li>
-                                    
-                                    
-                                </ul>
-                                <?php 
-                                     }?>
-                            </div>
-                            <div class="col-md-4">
-                                <ul class="list-group">
-                                
-                                    <li class="list-group-item  bg-secondary" aria-current="true">Departments</li>
-                                    <?php
-                                    include('connect.php');
-                                    $display = $con->prepare("SELECT * FROM hr_departments ORDER BY dept_id ASC");
-                                    $display->execute();
-                                    $fetch = $display->fetchAll();
+                            <li class="list-group-item  bg-secondary" aria-current="true">Departments</li>
+                            <?php
+                            include('connect.php');
+                            $display = $con->prepare("SELECT * FROM hr_departments ORDER BY dept_id ASC");
+                            $display->execute();
+                            $fetch = $display->fetchAll();
 
-                                    foreach($fetch as $key => $row) { ?>
-                                    <li class="list-group-item"><?php echo $row['dept_name']; ?></li>
-                                    
-                                    
-                                </ul>
-                                <?php 
-                                     }?>
-                            </div>
-                            <div class="col-md-4">
-                                <ul class="list-group">
-                                
-                                    <li class="list-group-item  bg-secondary" aria-current="true">Managers</li>
-                                    <?php
-                                    include('connect.php');
-                                    $display = $con->prepare("SELECT * FROM hr_manager ORDER BY mgr_id ASC");
-                                    $display->execute();
-                                    $fetch = $display->fetchAll();
+                            foreach ($fetch as $key => $row) { ?>
+                                <li class="list-group-item"><?php echo $row['dept_name']; ?></li>
 
-                                    foreach($fetch as $key => $row) { ?>
-                                    <li class="list-group-item"><?php echo $row['first_name'] ." ". $row['last_name']; ?></li>
-                                    
-                                    
-                                </ul>
-                                <?php 
-                                     }?>
-                            </div>
-                            <div class="card text-center mt-4">
-                                <div class="card-header">
-                                    HR APPLICATION FORM
-                                </div>
-                                <div class="card-body">
-                                    <p class="card-text">First you need to signup.</p>
-                                    <a href="signup.php" class="btn btn-primary">Register</a>
-                                    <a href="login.php" class="btn btn-primary">Apply Application</a>
 
-                                </div>
-                                <div class="card-body">
-                                    <a href="admin/" class="btn btn-primary">Administrator</a>
+                        </ul>
+                    <?php
+                            } ?>
+                    </div>
+                    <div class="col-md-4">
+                        <ul class="list-group">
 
-                                </div>
-                            </div>
+                            <li class="list-group-item  bg-secondary" aria-current="true">Managers</li>
+                            <?php
+                            include('connect.php');
+                            $display = $con->prepare("SELECT * FROM hr_manager ORDER BY mgr_id ASC");
+                            $display->execute();
+                            $fetch = $display->fetchAll();
+
+                            foreach ($fetch as $key => $row) { ?>
+                                <li class="list-group-item"><?php echo $row['first_name'] . " " . $row['last_name']; ?></li>
+
+
+                        </ul>
+                    <?php
+                            } ?>
+                    </div>
+                    <div class="card text-center mt-4">
+                        <div class="card-header">
+                            HR APPLICATION FORM
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">First you need to signup.</p>
+                            <a href="signup.php" class="btn btn-primary">Register</a>
+                            <a href="login.php" class="btn btn-primary">Apply Application</a>
 
                         </div>
-                        
+                        <div class="card-body">
+                            <a href="admin/" class="btn btn-primary">Administrator</a>
+
+                        </div>
                     </div>
+
                 </div>
 
             </div>
         </div>
-        <div class="container">
-        <div class="footer">
-            <h4 style="text-align: center;  ">Saurav Bhatt - 301227876 COMP214 2022
-            </h4>
-        </div>
+
     </div>
+</div>
+<div class="container">
+    <div class="footer">
+        <h4 style="text-align: center;  ">Saurav Bhatt - 301227876 COMP214 2022
+        </h4>
+    </div>
+</div>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $("#limit-records").change(function() {
+            $('form').submit();
+        })
+    })
+</script>
 </body>
 
 </html>
